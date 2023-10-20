6b76bcbb994d40de57c04c22f61e2a49caffed52
CandelarioRamosj <145868728+CandelarioRamosj@users.noreply.github.com>
2023-10-20 13:44:03 -0600

    script para calculadora se termino el script, dos botones estan desactivados

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
 let pantalla = document.getElementById('pantalla')
 let boton = document.querySelectorAll('.boton')
-
+let resultadoVar = ''
 for( let i = 0; i < boton.length; i++){
   boton[i].addEventListener('click', function(){
 
     switch (boton[i].textContent) {
-      case "AC": pantalla.value = ""
+      case "AC": pantalla.value = ''
+                  resultadoVar = ''
                   break;
-      case "←" : pantalla.value = pantalla.value.slice(0, pantalla.value.length -1);
+      case "←" :  error()
+                  pantalla.value = pantalla.value.slice(0, pantalla.value.length -1)
+                  resultadoVar = resultadoVar.slice(0, resultadoVar.length - 1)
                   break;
-      case "•" :  pantalla.value += "."
+      case "•" :  error()
+                  pantalla.value += '.'
+                  resultadoVar += '.'
                   break;
-      case "( )": parentesis()
+      case "( )": error()
+                  parentesis()
                   break;
-      case "=" :  resultado()
+      case "÷": error()
+                pantalla.value += '÷'
+                resultadoVar += '/'
                   break;
-      default: pantalla.value += boton[i].textContent
+      case "x": error()
+                pantalla.value += 'x'
+                resultadoVar +='*'
+                break;
+      case "−": error()
+                pantalla.value += '-'
+                resultadoVar += '-'
+                break;
+      case "=" :  error()
+                  resultado()
+                  break;
+      default: error()
+                pantalla.value += boton[i].textContent
+                resultadoVar += boton[i].textContent
                   break;
     }
     
 
   })
 }
-
+/*
 function parentesis() {
   if(pantalla.value.indexOf('(') === -1){
     pantalla.value += '('
-    
+    resultadoVar += '('
   }
   else if(pantalla.value.slice(-1) ==='('){
     pantalla.value += '('
+    resultadoVar += '('
   }
   else if(contarParentesis()){
     pantalla.value += ')'
+
   }
   else {
     pantalla.value +='('
+    resultadoVar += '('
   }
 }
+
 
 function contarParentesis(){
   let abrirParentesis = 0
@@ -57,6 +82,24 @@ function contarParentesis(){
     return true
   }
 }
+*/
+
+function error(){
+  if(pantalla.value == 'SyntaxError'){
+    pantalla.value = ''
+    resultadoVar = ''
+  }
+}
+
 function resultado(){
-  
+  try {
+    resultadoVar = eval(resultadoVar)
+    if(resultadoVar !== undefined){
+      pantalla.value = parseFloat(resultadoVar.toFixed(8))
+    }
+  } catch (error) {
+    pantalla.style.fontSize = '5vw' 
+    pantalla.value = 'SyntaxError'
+    resultadoVar = ''
+  }
 }
